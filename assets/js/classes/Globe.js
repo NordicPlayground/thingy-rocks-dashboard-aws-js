ccbdc7dd97f905751a8e8079b65b23c8a294ce14
alextryonpdx <alextryonpdx@gmail.com>
2020-09-16 18:56:09 -0700

    .

--- assets/js/classes/Globe.js
+++ assets/js/classes/Globe.js
@@ -7,10 +7,13 @@ class Globe {
     addPoint() {
         this.viewer.entities.add({
             position: Cesium.Cartesian3.fromDegrees(-122.6750, 45.5051),
-            point: {
-                pixelSize: 10,
-                color: Cesium.Color.YELLOW,
-            },
+            billboard: {
+                image: 'assets/img/nordic-icon-b.svg'
+            }
+            // point: {
+            //     pixelSize: 10,
+            //     color: Cesium.Color.YELLOW,
+            // },
         });
     }
 
