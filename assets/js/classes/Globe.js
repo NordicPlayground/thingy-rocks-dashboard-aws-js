daf7431759aeac46aa3aad0dcceda8b7522a1fd0
alextryonpdx <alextryonpdx@gmail.com>
2020-09-24 00:34:19 -0700

    fixes and a lot of untracked work

--- assets/js/classes/Globe.js
+++ assets/js/classes/Globe.js
@@ -15,10 +15,24 @@ class Globe {
     configScene(viewer) {
         //TODO the camera isn't right... need to control it more striclty and stop it from going off tilt or losing the globe
 
-        viewer.scene.screenSpaceCameraController.minimumZoomDistance = 50000;
-        viewer.scene.screenSpaceCameraController.maximumZoomDistance = 10000000;
+        // viewer.scene.screenSpaceCameraController.minimumZoomDistance = 500000;
+        // viewer.scene.screenSpaceCameraController.maximumZoomDistance = 90000000;
         // viewer.scene.screenSpaceCameraController._minimumZoomRate = 300;
         // viewer.scene.screenSpaceCameraController.enableTilt = false;
+
+        // var canvas = viewer.canvas;
+        // canvas.setAttribute('tabindex', '0'); // needed to put focus on the canvas
+        // canvas.onclick = function() {
+        //     canvas.focus();
+        // };
+        // var ellipsoid = viewer.scene.globe.ellipsoid;
+
+        // // disable the default event handlers
+        // viewer.scene.screenSpaceCameraController.enableRotate = false;
+        // viewer.scene.screenSpaceCameraController.enableTranslate = false;
+        // viewer.scene.screenSpaceCameraController.enableZoom = true;
+        // viewer.scene.screenSpaceCameraController.enableTilt = false;
+        // viewer.scene.screenSpaceCameraController.enableLook = true;
 
 
     }
@@ -44,7 +58,7 @@ class Globe {
         // console.log(dataPoints);
         // console.log(typeof(dataPoints));
         var deviceList = document.querySelector('.device-list');
-
+        //// The globe stopped rendering after changing to real data, and this specific loop...
         if (Object.keys(dataPoints).length > 1) {
             for (const device in dataPoints) {
                 console.log(typeof(dataPoints[device]))
@@ -56,21 +70,24 @@ class Globe {
 
 
     addPoint(viewer, data, deviceList) {
-        console.log(data);
-        console.log(viewer);
-        console.log(deviceList);
-        var listEntry = this.createListEntry(viewer, data, deviceList);
-        var entity = viewer.entities.add({
-            position: Cesium.Cartesian3.fromDegrees(data.position),
+        console.log(data.position);
+
+        // console.log(viewer);
+        // console.log(deviceList);
+        let listEntry = this.createListEntry(viewer, data, deviceList);
+        let entity = viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(data.position[1], data.position[0]),
             properties: {
                 name: data.properties.name,
                 coords: data.properties.coords,
                 list_entry: listEntry,
                 data: data.properties.data
             },
+            viewFrom: Cesium.Cartesian3.fromDegrees(data.position[1], data.position[0], 500000),
             // TODO: fix the icons and the sizing and clickable space
             billboard: {
-                image: 'assets/img/nordic-icon-b.svg'
+                rotation: 0,
+                image: 'assets/img/nordic-icon-b.svg',
             }
         });
         listEntry.addEventListener('click', function() {
@@ -105,7 +122,17 @@ class Globe {
 
                     listEntry.classList.add('active');
                 }
+                console.log(viewer.selectedEntity._position._value);
                 viewer.flyTo(viewer.selectedEntity);
+                // viewer.camera.setView({
+                //     // destination: { x: viewer.selectedEntity._position._value.x, y: viewer.selectedEntity._position._value.y, z: (viewer.selectedEntity._position._value.z + 500000) },
+                //     destination: Cesium.Cartesian3.fromDegrees(data.position[1], data.position[0])
+                //     orientation: {
+                //         heading: 0.0,
+                //         pitch: -Cesium.Math.PI_OVER_TWO,
+                //         roll: 0.0
+                //     }
+                // });
                 sidebar.openSidebar();
             } else {
                 sidebar.closeSidebar();
@@ -121,7 +148,7 @@ class Globe {
         var datumContainer = deviceData.querySelector('.device-data');
 
         name.innerHTML = props._name;
-        coords.innerHTML = props._coords;
+        coords.innerHTML = props._coords._value == null ? 'No GPS Data Available' : props._coords;
         // create datum blocks
         if (undefined !== props._data._value) {
             // clear out the data blocks in the sidebar
--- getDeviceData.php
+++ getDeviceData.php
@@ -49,9 +49,36 @@ function getDevices(){
 function getGPSforDevice($deviceID){
   $curlURL = "https://api.nrfcloud.com/v1/messages?inclusiveStart=2018-06-18T19%3A19%3A45.902Z&exclusiveEnd=3000-06-20T19%3A19%3A45.902Z&deviceIdentifiers=".$deviceID."&pageLimit=1&pageSort=desc&appId=GPS";
   $gps_message = sendCurl($curlURL);
-  $gps = isset($gps_message->items[0]) && !empty($gps_message->items[0]) ? $gps_message->items[0]->message->data : 'no gps data';
-    return $gps;
-  
+  $gps = isset($gps_message->items[0]) && !empty($gps_message->items[0]) ? $gps_message->items[0]->message->data : null;
+    // return $gps;
+    // echo '<pre>';
+    if($gps !== null ){
+      
+      $gps_array = explode(',', $gps);
+      // var_dump($gps_array);
+      $lat_multiplier = $gps_array[3] == 'N' ? 0.01 : -0.01;
+      $lat = floatval($gps_array[2]) * $lat_multiplier;
+      $lng_multiplier = $gps_array[5] == 'E' ? 0.01 : -0.01;
+      $lng = floatval($gps_array[4]) * $lng_multiplier;
+
+      // echo 'lng = ' . $lng;
+      // echo 'lat = ' . $lat;
+      $gps_readout = (floatval($gps_array[2]) * .01) . '° ' . $gps_array[3] . ', ' . (floatval($gps_array[4]) * .01) . '° ' . $gps_array[5];
+      $coords = array(
+        'lat' => $lat,
+        'lng' => $lng,
+        'readable' => $gps_readout
+      );
+      // var_dump($coords);
+      return $coords;
+    } else {
+      $coords = array(
+        'lat' => null,
+        'lng' => null,
+        'readable' => null
+      );
+    }
+
 }
 
 
@@ -63,20 +90,20 @@ function getMessagesForDevice($device){
   $curlURL = "https://api.nrfcloud.com/v1/messages?inclusiveStart=2018-06-18T19%3A19%3A45.902Z&exclusiveEnd=3000-06-20T19%3A19%3A45.902Z&deviceIdentifiers=".$deviceID."&pageLimit=100&pageSort=desc";
 
   $messages = sendCurl($curlURL);
-
+  $gps_data = getGPSforDevice($deviceID);
   $device = array(
-    'position' => '-122.6750, 48.5051',
+    'position' => array($gps_data['lat'], $gps_data['lng']),
     'properties' => array(
-      'coords' => getGPSforDevice($deviceID),
+      'coords' => $gps_data['readable'],
       'name' => $name,
       'data' => array(
         'temp' => false,
-        'air_pressure' => false,
+        // 'air_pressure' => false,
         'humidity' => false
       )
     ),
     'id' => $deviceID,
-    'gps' => getGPSforDevice($deviceID),
+    'gps' => $gps_data,
   );
 
   foreach($messages->items as $item){
