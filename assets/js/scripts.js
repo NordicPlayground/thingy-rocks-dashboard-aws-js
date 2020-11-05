a5c5209f4ad3c8617595ec05e5a31786918c7036
alextryonpdx <alextryonpdx@gmail.com>
2020-11-04 22:41:55 -0800

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,24 +20,24 @@ const handleEvent = (eventName, {
 }
 
 
-function loadScript(src, callback) {
-    var s,
-        r,
-        t;
-    r = false;
-    s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = src;
-    s.onload = s.onreadystatechange = function() {
-        //console.log( this.readyState ); //uncomment this line to see which ready states are called.
-        if (!r && (!this.readyState || this.readyState == 'complete')) {
-            r = true;
-            callback();
-        }
-    };
-    t = document.getElementsByTagName('script')[0];
-    t.parentNode.insertBefore(s, t);
-}
+// function loadScript(src, callback) {
+//     var s,
+//         r,
+//         t;
+//     r = false;
+//     s = document.createElement('script');
+//     s.type = 'text/javascript';
+//     s.src = src;
+//     s.onload = s.onreadystatechange = function() {
+//         //console.log( this.readyState ); //uncomment this line to see which ready states are called.
+//         if (!r && (!this.readyState || this.readyState == 'complete')) {
+//             r = true;
+//             callback();
+//         }
+//     };
+//     t = document.getElementsByTagName('script')[0];
+//     t.parentNode.insertBefore(s, t);
+// }
 
 function getData() {
     var xmlhttp = new XMLHttpRequest();
