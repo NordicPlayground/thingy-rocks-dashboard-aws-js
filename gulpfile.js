3a903ef2ca92f049ec862a5e23fefccaafb183bb
MagWeaver <cmagweaver@gmail.com>
2022-06-29 15:10:33 -0700

    chore: compile sass files correctly into the build

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, series } = require("gulp");
 const rename = require("gulp-rename");
-const uglify = require('gulp-uglify-es').default;
+const uglify = require("gulp-uglify-es").default;
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
 
 function build(cb) {
-	src(['assets/favicon.png', 'assets/index.html'])
-		.pipe(dest('dist/'));
+  src(["assets/favicon.png", "assets/index.html"]).pipe(dest("dist/"));
 
-	src(['assets/js/classes/*.js', 'assets/js/*.js'])
-		.pipe(uglify())
-		.pipe(rename(function(path){
-			path.basename += '-min'
-		}))
-		.pipe(dest('dist/js/'));
+  src(["assets/js/classes/*.js", "assets/js/*.js"])
+    .pipe(uglify())
+    .pipe(
+      rename(function (path) {
+        path.basename += "-min";
+      })
+    )
+    .pipe(dest("dist/js/"));
 
-	src(['assets/js/Cesium-1.75/Build/Cesium/**'])
-		.pipe(dest('dist/js/Cesium'));
+  src(["assets/js/Cesium-1.75/Build/Cesium/**"]).pipe(dest("dist/js/Cesium"));
 
-	src('assets/img/**')
-		.pipe(dest('dist/img/'));
+  src("assets/img/**").pipe(dest("dist/img/"));
 
-	src('assets/css/**')
-		.pipe(dest('dist/css/'))
+  src("assets/fonts/**").pipe(dest("dist/fonts"));
 
-	cb();
+  src("assets/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("styles-min.css"))
+    .pipe(cleanCSS())
+    .pipe(dest("dist/"));
+
+  cb();
 }
 
-//TODO: include linter
 exports.default = series(build);
