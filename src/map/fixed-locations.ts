ab9c808bc11e9fec819286803745e1eeec87699f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-08 12:16:25 +0100

    fix(mesh): move gateway a little bit

--- src/map/fixed-locations.ts
+++ src/map/fixed-locations.ts
@@ -1,6 +1,6 @@
 // Wirepas 5G Mesh Gateway, Embedded World
 export const Wirepas5GMeshGatewayLocation = {
-	lat: 49.41866700656591,
-	lng: 11.120463809846152,
+	lat: 49.418762145165005,
+	lng: 11.120772573937822,
 	label: 'Hall 4, Booth #4-580',
 } as const
