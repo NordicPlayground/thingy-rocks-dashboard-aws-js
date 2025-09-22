2e2a3964fcbd2e2ad25f8d7a21b08386d4c63a1f
Lena Kråkevik Haraldseid <35926103+Lenakh97@users.noreply.github.com>
2025-09-22 15:22:29 +0200

    fix: use DeviceName element to show name (#87)

--- src/nordicNRPlus/NordicNrplusNetworkTile.tsx
+++ src/nordicNRPlus/NordicNrplusNetworkTile.tsx
@@ -201,7 +201,7 @@ export const NordicNrplusNetworkTile = ({
 		<>
 			<Title onClick={handleClick}>
 				<NRPlus class="icon" />
-				<span>{sinkDevice?.id.slice(-4)}</span>
+				{sinkDevice && <DeviceName device={sinkDevice} />}
 				<SinkInfo>
 					{lastUpdateTime !== undefined && (
 						<LastUpdate title="Last update">
