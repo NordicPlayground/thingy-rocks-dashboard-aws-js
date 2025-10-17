5d46bc3d078709ab2e9d7d1e6c7d7add3e2d3e5e
Lena Kråkevik Haraldseid <35926103+Lenakh97@users.noreply.github.com>
2025-10-17 08:32:59 +0200

    fix: use RelativeTime instead of GeoLocationAge (#93)
    This fixes the issue that on some systems the number becomes
    negative which is caused by https://github.com/date-fns/date-fns/issues/3238
    
    Closes #92
    
    Co-authored-by: Markus Tacker <Markus.Tacker@NordicSemi.no>

--- src/RelativeTime.tsx
+++ src/RelativeTime.tsx
@@ -2,20 +2,26 @@ import { useEffect, useState } from 'preact/hooks'
 
 const formatDistance = (time: Date): string => {
 	const seconds = Math.floor((Date.now() - time.getTime()) / 1000)
-	if (seconds < 60) return `${seconds} s`
+	if (seconds < 60) return `${seconds} ${seconds === 1 ? 'sec' : 'secs'}`
 	const minutes = Math.floor(seconds / 60)
-	if (minutes < 60) return `${minutes} m`
+	if (minutes < 60) return `${minutes} ${minutes === 1 ? 'min' : 'mins'}`
 	const hours = Math.floor(minutes / 60)
-	return `${hours} h`
+	return `${hours} ${hours === 1 ? 'hour' : 'hours'}`
 }
 
-export const RelativeTime = ({ time }: { time: Date }) => {
+export const RelativeTime = ({
+	time,
+	updatedIntervalSeconds = 1,
+}: {
+	time: Date
+	updatedIntervalSeconds?: number
+}) => {
 	const [formatted, setFormatted] = useState<string>(formatDistance(time))
 
 	useEffect(() => {
 		const i = setInterval(() => {
 			setFormatted(formatDistance(time))
-		}, 1000)
+		}, 1000 * updatedIntervalSeconds)
 
 		return () => {
 			clearInterval(i)
